c9ebbaef6c30a3d17b61f33a876b96bb0c62e4f1
KirubelEnyew <kirastarboys@gmail.com>
2022-05-24 11:28:39 +0300

    feat - added error handlng middleware and routes

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors')
+const employee = require('./Api/Routes/employeeRoutes')
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+// routes here
+app.use('/api/employee', employee);
+
+// to handle unkown route requests
+app.use('*', (req, res) => {
+    return res.status(404).json({
+        error: true,
+        message: 'Error, Request Not Found',
+        data: null
+    })
+});
+
+// to handle all errors
+app.use((error, req, res, next) => {
+    res.status(500).json({ error: error.message });
+})
+
+module.exports = app;
